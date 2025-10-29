352ece801c4baf00762daae79eda11b7228a83f8
Based <notyuldshah@gmail.com>
2025-10-29 19:33:17 +0500

    use the latest webapp version

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className}>
       <head>
-        <Script src="https://telegram.org/js/telegram-web-app.js?59" strategy="beforeInteractive" />
+        <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
       </head>
       <body className="antialiased">
         <ErrorBoundary>
